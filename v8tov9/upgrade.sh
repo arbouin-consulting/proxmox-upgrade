e88fd8b96c6fc32ecc1c5a4aedcbccceb8c5ee6e
Kevin ARBOUIN <lesmyrmidons@gmail.com>
2025-08-27 13:36:20 +0200

    chore: update repository and remove old repository

--- v8tov9/upgrade.sh
+++ v8tov9/upgrade.sh
@@ -6,14 +6,34 @@ test () {
 
 upgrade () {
   echo "Remove package systemd-boot"
-  apt remove --purge systemd-boot linux-image-amd64
+  apt remove --purge systemd-boot linux-image-amd64 -y
   apt update
   apt dist-upgrade -y
   apt autoremove -y
   pveversion
 
+  echo "Change and create new repository of bookworm to trixie"
   sed -i 's/bookworm/trixie/g' /etc/apt/sources.list
   sed -i 's/bookworm/trixie/g' /etc/apt/sources.list.d/pve-enterprise.list
+  cat > /etc/apt/sources.list.d/ceph.sources << EOF
+Types: deb
+URIs: http://download.proxmox.com/debian/ceph-squid
+Suites: trixie
+Components: no-subscription
+Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
+EOF
+  cat > /etc/apt/sources.list.d/proxmox.sources << EOF
+Types: deb
+URIs: http://download.proxmox.com/debian/pve
+Suites: trixie
+Components: pve-no-subscription
+Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
+EOF
+
+  echo "Clean old repository"
+  rm -f /etc/apt/sources.list.d/ceph.list
+  rm -f /etc/apt/sources.list.d/pve-install-repo.list
+  rm -f /etc/apt/sources.list.d/pve-enterprise.list
 
   apt update
 
